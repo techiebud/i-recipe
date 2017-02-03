3eed3ea41daa5c32ed4cb020fc5158c027a95aed
Techiebud <techiebud@gmail.com>
2017-02-03 10:20:45 -0500

    Lesson 110

---
+++ src/services/shopping-list.ts
@@ -0,0 +1,27 @@
+import { Ingredient } from './../models/ingredient';
+
+export class ShoppingListSerice {
+
+   private ingredients: Ingredient[] = [];
+
+   addItem(name: string, amount: number) {
+        this.ingredients.push(new Ingredient(name, amount));       
+       
+   }
+
+   addItems(items: Ingredient[])
+  {
+      this.ingredients.push(...items);
+  }
+
+  getItems() {
+
+      return this.ingredients.slice();
+  }
+
+  removeItem(index: number) {
+
+      this.ingredients.splice(index, index);
+  }
+    
+}
