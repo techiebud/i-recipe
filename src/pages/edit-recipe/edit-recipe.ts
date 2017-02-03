6865afe7a9353b5d5a019d9d047139ed0de20e3a
Techiebud <techiebud@gmail.com>
2017-02-03 13:13:50 -0500

    Lesson 116

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-import { NavParams } from 'ionic-angular';
+import { NavParams } from 'ionic-angular'
 
 @Component({
   selector: 'page-edit-recipe',
@@ -9,11 +10,22 @@ import { NavParams } from 'ionic-angular';
 export class EditRecipePage {
   mode = 'New';
   selectOptions = ['Easy', 'Medium', 'Hard'];
+  receipeForm: FormGroup;
   constructor(private navParams: NavParams) {}
 
   ngOnInit() {
 
     this.mode = this.navParams.get("mode");
+  }
+
+  private initializeForm() {
+
+      this.receipeForm = new FormGroup({
+          'title': new FormControl(null, Validators.required),
+          'description': new FormControl(null, Validators.required), 
+          'difficulty': new FormControl('Medium', Validators.required)
+        
+      });
   }
 
 }
