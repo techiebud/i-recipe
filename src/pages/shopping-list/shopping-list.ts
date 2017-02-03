2b22c7ea0694ba3977acd2a196a7274817bac7fd
Techiebud <techiebud@gmail.com>
2017-02-03 09:29:50 -0500

    Lesson 107

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,22 +1,17 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NgForm } from "@angular/forms";
 
-/*
-  Generated class for the ShoppingList page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html'
 })
 export class ShoppingListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor() {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ShoppingListPage');
+  onAddItem(form: NgForm) {
+    console.log(form);
+    
   }
 
 }
