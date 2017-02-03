22e76096f690f0a3b4bdb48648f9e01d86e7fb85
Techiebud <techiebud@gmail.com>
2017-02-03 07:12:45 -0500

    Lecture 100 changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
+import { Splashscreen, StatusBar } from 'ionic-native';
+
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
-
-import { HomePage } from '../pages/home/home';
-
+import { TabsPage } from './../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = HomePage;
+  rootPage = TabsPage
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,22 +1,16 @@
-import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the Tabs page.
+import { Component } from '@angular/core';
+import { RecipesPage } from './../recipes/recipes';
+import { ShoppingListPage } from './../shopping-list/shopping-list';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TabsPage');
-  }
+  slPage = ShoppingListPage;
+  recipesPage = RecipesPage;
+  
 
 }
