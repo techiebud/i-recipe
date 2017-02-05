8ee513729e3d07fff311f8bff9e2db69cf27717d
Techiebud (Mac) <techiebud@gmail.com>
2017-02-05 09:46:41 -0500

    Lesson 131

--- src/pages/recipe/recipe.ts
+++ src/pages/recipe/recipe.ts
@@ -1,31 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
 
-import { NavParams } from 'ionic-angular';
+import { EditRecipePage } from './../edit-recipe/edit-recipe';
 import { Recipe } from './../../models/recipe';
 
 @Component({
   selector: 'page-recipe',
   templateUrl: 'recipe.html'
 })
-export class RecipePage implements OnInit{
+export class RecipePage implements OnInit {
 
   recipe: Recipe;
   index: number;
-  constructor(private navParams: NavParams) {
-    
-    
+  constructor(private navCtrl: NavController, private navParams: NavParams) {
+
+
   }
 
   ngOnInit() {
 
-     this.recipe = this.navParams.get("recipe");
-     console.log("recipe:");
-     console.log(this.recipe);
-     this.index = this.navParams.get("index");
-     
-     
+    this.recipe = this.navParams.get("recipe");
+    console.log("recipe:");
+    console.log(this.recipe);
+    this.index = this.navParams.get("index");
+
+
   }
-  
+
+  onEditRecipe() {
+    this.navCtrl.push(EditRecipePage, { mode: 'Edit', recipe: this.recipe, index: this.index });
+  }
+
+
 
 
 }
