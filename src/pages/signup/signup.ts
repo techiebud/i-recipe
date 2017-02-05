9a8d92be5ed5adc24f1e318fc9fb211b5609d615
Techiebud (Mac) <techiebud@gmail.com>
2017-02-05 15:57:48 -0500

    Lesson 148

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,3 +1,5 @@
+import { AlertController, LoadingController } from "ionic-angular";
+
 import { AuthService } from './../../services/auth';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
@@ -8,15 +10,32 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupPage {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private loadingCtrl: LoadingController, private alertCtrl: AlertController) {}
 
    onSignup(form: NgForm) {
     // console.debug("onSignUp");
      //onsole.log(form);
+     const loading = this.loadingCtrl.create({
+        content: "Signing you up...." 
+     });
+     loading.present();   
+     
+
      this.authService.signUp(form.value.email, form.value.password)
-      .then(data => console.log(data))
-      .catch(error => console.log(error));
-            
+      .then(data => {
+          loading.dismiss();
+          console.log(data);
+      })
+      .catch(error => {
+          loading.dismiss();
+          const alert = this.alertCtrl.create({
+             title: 'Signup failed!',
+             message: error.message,
+             buttons: ['Ok']
+          })
+          console.error(error); 
+          alert.present();
+      })
       
       
      
