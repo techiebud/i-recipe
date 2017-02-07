5655fbc6eb529773484b76af565477122a50df01
Techiebud <techiebud@gmail.com>
2017-02-07 10:17:48 -0500

    Lesson 159

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,3 +1,5 @@
+import { AlertController, LoadingController } from 'ionic-angular';
+
 import { AuthService } from './../../services/auth';
 import { Component } from '@angular/core';
 import { Ingredient } from './../../models/ingredient';
@@ -16,7 +18,9 @@ export class ShoppingListPage {
   listItems: Ingredient[];
   constructor(private slService: ShoppingListService,
     private popoverCtrl: PopoverController,
-    private authService: AuthService) { }
+    private authService: AuthService,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController) { }
 
   onAddItem(form: NgForm) {
     this.slService.addItem(form.value.ingredientName, form.value.amount);
@@ -41,61 +45,71 @@ export class ShoppingListPage {
   }
 
   onShowOptions(event: MouseEvent) {
-
+    const loading = this.loadingCtrl.create( {
+        content: 'Please wait....'
+    })
     const popover = this.popoverCtrl.create(SLOptionsPage);
     popover.present({ ev: event });
     popover.onDidDismiss(
       data => {
-
         if (!data) {
           return;
         }
         if (data.action == 'load') {
-             this.authService.getActiveUser().getToken()
-            .then((token: string) => {
-              this.slService.fetchList(token)
-                .subscribe(
-                (list: Ingredient[]) => {
-                  if (list)
-                  {
-                     this.listItems = list;
-                  }
-                  else {
-                    this.listItems = [];
-                  }
-                 
-                },
-                
-                (error) => {
-                  console.log(error);
-                }
-                )
-            })
-            .catch(error => {
-              console.error(error);
-            })
-
-        }
-        else {
+          loading.present();
           this.authService.getActiveUser().getToken()
-            .then((token: string) => {
-              this.slService.storeList(token)
-                .subscribe(
-                () => console.log('Success!'),
-                (error) => {
-                  console.log(error);
-                }
-                )
-            })
-            .catch(error => {
-              console.error(error);
-            })
+            .then(
+              (token: string) => {
+                this.slService.fetchList(token)
+                  .subscribe(
+                    (list: Ingredient[]) => {
+                      loading.dismiss();
+                      if (list) {
+                        this.listItems = list;
+                      } else {
+                        this.listItems = [];
+                      }
+                    },
+                    error => {
+                      loading.dismiss();
+                      console.error(error);
+                      this.handleError(error.json().error);
+                    }
+                  );
+              }
+            );
+        } else if (data.action == 'store') {
+          loading.present();
+          this.authService.getActiveUser().getToken()
+            .then(
+              (token: string) => {
+                this.slService.storeList(token)
+                  .subscribe(
+                    () => loading.dismiss(),
+                    error => {
+                      loading.dismiss();
+                      console.error(error);
+                      this.handleError(error.json().error);
+                    }
+                  );
+              }
+            );
         }
-
       }
+    ); 
 
-    )
 
+  }
 
+  private handleError(errorMessage) 
+  {
+      const alert = this.alertCtrl.create({
+
+         title: "An error occurred!",
+         message: errorMessage,
+         buttons: ["OK"]
+      })
+      alert.present();
+    
   }
 }
